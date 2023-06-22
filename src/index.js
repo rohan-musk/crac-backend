1ea6f421a997adf038182e9e382038fa7a85e446
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2023-06-22 18:53:38 +0530

    RM| Add mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
