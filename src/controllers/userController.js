40f1bb78c69fe097418b6e12801fbf185c232ef7
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2023-07-01 14:58:33 +0530

    RM|Minor changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,5 +2,5 @@ const UserData = require('../models/userDataModel');
 
 exports.getUsers = async (req, res, next) => {
   const allUsers = await UserData.find({});
-  return res.send({ allUsers });
+  return res.status(200).send({ allUsers });
 };
