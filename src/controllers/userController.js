e36e0baf1f8cc3423bc3079bb72b14a9d32b1621
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2023-08-10 17:44:43 +0530

    RM| Add route for specific user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,3 +4,8 @@ exports.getUsers = async (req, res, next) => {
   const allUsers = await UserData.find({});
   return res.status(200).send({ allUsers });
 };
+
+exports.getSpecificUser = async (req, res, next) => {
+  const user = await UserData.find({id:req.params.id});
+  return res.status(200).send({ user });
+};
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 
 router.get('/getUsers', userController.getUsers);
+router.get('/getUsers/:id',userController.getSpecificUser);
 
 module.exports = router;
