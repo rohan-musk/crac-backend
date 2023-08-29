f3610556763e1d866c356343e1254c75734e1074
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2023-08-29 11:08:01 +0530

    RM| Add edit artist about

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -10,6 +10,25 @@ exports.getSpecificArtist = async (req, res, next) => {
       data: { message: 'No artist data found' },
     });
 };
-exports.editArtistAbout = async (req, res, next) => {};
+exports.editArtistAbout = async (req, res, next) => {
+  const data = await ArtistData.findOneAndUpdate(
+    { id: req.params.id },
+    { aboutMe: req.body.aboutme }
+  );
+  if (data)
+    return res
+      .status(200)
+      .send({
+        status: 'success',
+        data: { message: 'Succesfully updated about' },
+      });
+  else
+    return res.status(400).send({
+      status: 'failed',
+      data: {
+        message: 'Could not change data cause no artist with that ID exists.',
+      },
+    });
+};
 exports.editArtistInsta = async (req, res, next) => {};
 exports.editArtistEmail = async (req, res, next) => {};
--- src/routes/artistRouter.js
+++ src/routes/artistRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const artistController = require('../controllers/artistController');
 
 router.get('/getArtist/:id', artistController.getSpecificArtist);
-router.get('/editArtistAbout/:id', artistController.editArtistAbout);
+router.patch('/editArtistAbout/:id', artistController.editArtistAbout);
 router.get('/editArtistInsta/:id', artistController.editArtistInsta);
 router.get('/editArtistEmail/:id', artistController.editArtistEmail);
 
