e269ced986af0fcc9ee767b4fbd84cee8dbc7b90
Rohan Muskawad <84379921+rohan-musk@users.noreply.github.com>
2023-08-29 13:24:49 +0530

    RM| Route protected

--- src/routes/artistRouter.js
+++ src/routes/artistRouter.js
@@ -2,8 +2,18 @@ const router = require('express').Router();
 const artistController = require('../controllers/artistController');
 
 router.get('/getArtist/:id', artistController.getSpecificArtist);
+
+router.use((req, res, next) => {
+  if (req.session.user) next();
+  else
+    res.status(401).send({
+      status: 'failed',
+      message: 'User anauthorised',
+    });
+});
+
 router.patch('/editArtistAbout/:id', artistController.editArtistAbout);
-router.get('/editArtistInsta/:id', artistController.editArtistInsta);
-router.get('/editArtistEmail/:id', artistController.editArtistEmail);
+router.patch('/editArtistInsta/:id', artistController.editArtistInsta);
+router.patch('/editArtistEmail/:id', artistController.editArtistEmail);
 
 module.exports = router;
